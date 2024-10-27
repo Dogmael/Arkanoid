6e7c015a5d859411069b779165798f5771a56a82
Dogmaël DARTIGUE-PEYROU <dogmaeldogmael@gmail.com>
2024-10-27 09:33:26 +0100

    Desactivation transpilling with babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ export default {
 	},
 	module: {
 		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
+			// {
+			// 	test: /\.js$/,
+			// 	exclude: /node_modules/,
+			// 	use: {
+			// 		loader: 'babel-loader'
+			// 	}
+			// },
 			{
 				test: /\.css$/,
 				use: [MiniCssExtractPlugin.loader, 'css-loader']
