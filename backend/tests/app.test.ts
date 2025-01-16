b4a32700c337af17363f5b5d21f7994b54a52b14
Dogmaël DARTIGUE-PEYROU <dogmaeldogmael@gmail.com>
2025-01-16 23:09:41 +0100

    Run test on docker temporary Postgresl database

--- backend/tests/app.test.ts
+++ backend/tests/app.test.ts
@@ -4,9 +4,11 @@ import buildApp from '../src/app';
 const app = buildApp();
 describe('POST /users', () => {
 	it('should create a new user', async () => {
+		console.log('DATABASE_URL =', process.env.DATABASE_URL);
+
 		const newUser = {
-			userName: 'testuser2',
-			email: 'testuser2@example.com'
+			userName: 'testuser',
+			email: 'testuser@example.com'
 		};
 
 		const response = await app.inject({
@@ -20,19 +22,5 @@ describe('POST /users', () => {
 		expect(response.json()).toHaveProperty('pkUser');
 		expect(response.json().userName).toBe(newUser.userName);
 		expect(response.json().email).toBe(newUser.email);
-	});
-
-	it('should return 400 if required fields are missing', async () => {
-		const response = await app.inject({
-			method: 'POST',
-			url: '/users',
-			payload: {
-				userName: 'testuser3'
-			}
-		});
-
-		expect(response.statusCode).toBe(400);
-		expect(response.headers['content-type']).toMatch(/json/);
-		expect(response.json()).toHaveProperty('message');
 	});
 });
