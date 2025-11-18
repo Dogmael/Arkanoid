7de4237cd3354e61cc192e2c69674fe6312044db
Dogmaël DARTIGUE-PEYROU <dogmaeldogmael@gmail.com>
2025-11-18 22:37:45 +0100

    [BACK] Ajout des plugins cors et helmet et authoriser cross origin pour mon front (dev uniquement pour l'instant)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,5 @@
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
 import Fastify, { FastifyServerOptions } from 'fastify';
 import prismaPlugin from './plugins/prismaPlugin';
 import usersRoute from './routes/usersRoute';
@@ -7,6 +9,10 @@ export default function buildApp (options: FastifyServerOptions = {}) {
 
 	// Register plugins
 	fastify.register(prismaPlugin);
+	fastify.register(helmet);
+	fastify.register(cors, {
+		origin: 'http://localhost:8080', // Dev config
+	});
 
 	// Register routes
 	fastify.register(usersRoute);
